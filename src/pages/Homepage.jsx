782b65db679fc19a772dbe3345e5a8fc129e7a6f
Dipesh <dipeshrjk27@gmail.com>
2025-04-14 20:07:03 +0530

    changed cta

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
   return (
     <div
       className="min-h-screen bg-black text-white flex flex-col bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('src/assets/ds.png')" }}
+      style={{ backgroundImage: "url('https://media-hosting.imagekit.io/8bfd284a18a24e75/ds.png?Expires=1839249320&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=hFebof4BsovkAe-i4atZ1Z4FATpFJLjnNEtm8MHYyRoEJC7Tx2WS7LTs66NS0gFf4AsqbVcbZvbO7-ue9dyhIlEQJjZvL2W~PGz4sSsaPyig1p0xFzQEID8wpmh5G2ChxutKvb1KY9PgO0A5DWp7sBCNbrtQSBcauF5V73o0EGsAktTJHnFHGSwZ9-13uSIUP2D1OP0cCICct2WYiNIL5pA3CYnim9RBJhsD0BYkB5d0OnTDQ8Q74KSPaQhE6TLeSG6cygffEbyIgm8ReHUQ~ksS0CNEVygb7jlldtoGc59A~xNcclstDmSDGg4Ts7CfbdniqZxCZq-WPdOCqSlctQ__')" }}
     >
       <Header />
       <main className="flex flex-col items-center text-center px-6 md:px-20 py-12 md:py-24 space-y-10 backdrop-blur-sm bg-black/70">
@@ -37,7 +37,7 @@ const Homepage = () => {
 
         <motion.video 
           className="w-full max-w-5xl aspect-video rounded-xl shadow-xl border border-gray-700"
-          src="src/assets/hero-video.mp4"
+          src="https://media-hosting.imagekit.io/5e383f5a1cd44628/hero-video.mp4?Expires=1839249320&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Hvw8x0uwN1bwLhf0AvsoZx32pST-NTokh98eeJsi72eVnF9PGvKj8ib9TPQUSks5k~tCrlxHXZRHu-SxBdRGnzca2ADaawur0-qSTBar-w7N8XLUi6bLVxBk5~BMCqgZnQkwYShCEJqseiDsQ0YrFjgjj3S6sfB0FhJtGu9T2pS~mvB8gb5QLp2gLLw9WS49fKH09fVijTkOVfmnLk12d4EiB1ICmuqZKWbZ2qLt4FYh5TmMLA5BUSQpwIoAQLi4bKeA~I0F7hf09m00qNj3cDMJ6lio13odSjwAHH6aLto3ntrj8xS8AzeVNioddoGMziUZU7XeHrccCY~gsWkuSg__"
           autoPlay loop muted playsInline
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
